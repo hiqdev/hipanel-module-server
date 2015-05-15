37b3e0dbfe8946d0b1c4fb721d8c8bdb2b214f45
Andrii Vasyliev <sol@hiqdev.com>
2015-05-15 15:46:27 +0000

    + Plugin, * Menu

---
+++ Plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link    http://hiqdev.com/hipanel-module-server
+ * @license http://hiqdev.com/hipanel-module-server/license
+ * @copyright Copyright (c) 2015 HiQDev
+ */
+
+namespace hipanel\modules\server;
+
+class Plugin extends \hiqdev\pluginmanager\Plugin
+{
+    protected $_items = [
+        'menus' => [
+            [
+                'class' => 'hipanel\modules\server\SidebarMenu',
+            ],
+        ],
+        'modules' => [
+            'server' => [
+                'class' => 'hipanel\modules\server\Module',
+            ],
+        ],
+    ];
+
+}
---
+++ SidebarMenu.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @link    http://hiqdev.com/hipanel-module-server
+ * @license http://hiqdev.com/hipanel-module-server/license
+ * @copyright Copyright (c) 2015 HiQDev
+ */
+
+namespace hipanel\modules\server;
+
+class SidebarMenu extends \hipanel\base\Menu implements \yii\base\BootstrapInterface
+{
+
+    protected $_addTo = 'sidebar';
+
+    protected $_where = [
+        'after'     => ['domains', 'tickets', 'finance', 'clients', 'dashboard'],
+        'before'    => ['hosting'],
+    ];
+
+    protected $_items = [
+        'servers' => [
+            'label' => 'Servers',
+            'url'   => ['/server/server/index'],
+            'icon'  => 'fa-server',
+            'items' => [
+                'servers' => [
+                    'label' => 'Servers',
+                    'url'   => ['/server/server/index'],
+                    'icon'  => 'fa-server',
+                ],
+            ],
+        ],
+    ];
+
+}
