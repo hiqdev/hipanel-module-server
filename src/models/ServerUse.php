96bfe22188433a34495d8808ea4c96523efe9444
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-02-23 18:20:15 +0200

    Added ServerUse model

---
+++ src/models/ServerUse.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link    http://hiqdev.com/hipanel-module-server
+ * @license http://hiqdev.com/hipanel-module-server/license
+ * @copyright Copyright (c) 2015 HiQDev
+ */
+
+namespace hipanel\modules\server\models;
+
+use hipanel\modules\finance\models\RUse;
+use Yii;
+use yii\base\InvalidParamException;
+
+class ServerUse extends RUse
+{
+    use \hipanel\base\ModelTrait;
+
+    private function getTrafficTypes()
+    {
+        return ['server_traf_in', 'server_traf_max', 'server_traf'];
+    }
+
+    private function getBandwidthTypes()
+    {
+        return ['server_traf95_in', 'server_traf95_max', 'server_traf95'];
+    }
+
+    public function getDisplayAmount()
+    {
+        if (in_array($this->type, $this->getBandwidthTypes())) {
+            return round($this->last / 10**6, 2);
+        } elseif (in_array($this->type, $this->getTrafficTypes())) {
+            return round($this->total / 10**9, 2);
+        }
+
+        return $this->total;
+    }
+}
