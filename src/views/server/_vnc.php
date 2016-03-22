bfb2e869f3bd3ee453017d974194c801b21fab66
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-03-22 19:03:24 +0200

    server/view - VNC wrapped in a PJAX call

--- src/views/server/_vnc.php
+++ src/views/server/_vnc.php
@@ -1,5 +1,12 @@
 <?php
+use hipanel\widgets\Pjax;
 use yii\helpers\Html;
+
+Pjax::begin([
+    'id' => 'server-vnc-pjax',
+    'enablePushState' => false,
+    'enableReplaceState' => false,
+]);
 
 if ($model->vnc['enabled']) {
     echo Html::tag('span',
@@ -7,30 +14,31 @@ if ($model->vnc['enabled']) {
         ['class' => 'label label-success']);
 
     $fields = [
-        'IP'       => $model->vnc['vnc_ip'],
-        'Port'     => $model->vnc['vnc_port'],
-        'Password' => $model->vnc['vnc_password']
+        Yii::t('hipanel/server', 'IP') => $model->vnc['vnc_ip'],
+        Yii::t('hipanel/server', 'Port') => $model->vnc['vnc_port'],
+        Yii::t('hipanel/server', 'Password') => $model->vnc['vnc_password']
     ];
     ?>
     <dl class="dl-horizontal">
         <?php foreach ($fields as $name => $value) { ?>
             <dt><?= $name ?></dt>
             <dd><?= $value ?></dd>
-        <?php
+            <?php
         } ?>
     </dl>
     <?php if (!empty($model->vnc['endTime']) && $model->vnc['endTime'] > time()) {
-        echo Yii::t('hipanel/server', 'VNC will be disabled {time}', ['time' => Yii::$app->formatter->asRelativeTime($model->vnc['endTime'])]);
+        echo Yii::t('hipanel/server', 'VNC will be disabled {time}',
+            ['time' => Yii::$app->formatter->asRelativeTime($model->vnc['endTime'])]);
     }
 } else {
     echo Html::beginForm(['enable-vnc', 'id' => $model->id], "POST", ['data' => ['pjax' => 1], 'class' => 'inline']);
     echo Html::submitButton(
         Yii::t('hipanel/server', 'Enable'),
         [
-            'class'             => 'btn btn-success',
+            'class' => 'btn btn-success',
             'data-loading-text' => Yii::t('hipanel/server', 'Enabling...'),
-            'onClick'           => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')"),
-            'disabled'          => !$model->isOperable() || !$model->isVNCSupported(),
+            'onClick' => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')"),
+            'disabled' => !$model->isOperable() || !$model->isVNCSupported(),
         ]
     );
     echo ' ';
@@ -39,3 +47,5 @@ if ($model->vnc['enabled']) {
     }
     echo Html::endForm();
 }
+
+Pjax::end();
