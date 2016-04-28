e293d58d92906f1b3fe001158d08310aeebd7dd4
Andrii Vasyliev <sol@hiqdev.com>
2016-04-28 15:28:30 +0000

    added tests

---
+++ tests/unit/controllers/ServerControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Finance module for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-server
+ * @package   hipanel-module-server
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\server\tests\unit\controllers;
+
+use hipanel\modules\server\controllers\ServerController;
+
+class ServerControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ServerController
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+        $this->object = new ServerController('test', null);
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    public function testActions()
+    {
+        $this->assertInternalType('array', $this->object->actions());
+    }
+}
