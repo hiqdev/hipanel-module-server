0d682933ebbd4fd434f3dade5f9d13b108fa45d6
Andrii Vasyliev <sol@hiqdev.com>
2016-05-18 14:36:53 +0000

    redone to composer-config-plugin

---
+++ src/config/hisite.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+* Server module for HiPanel
+*
+* @link      https://github.com/hiqdev/hipanel-module-server
+* @package   hipanel-module-server
+* @license   BSD-3-Clause
+* @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+*/
+
+return [
+    'aliases' => [
+        '@server' => '/server/server',
+        '@rrd' => '/server/rrd',
+        '@switch-graph' => '/server/switch-graph',
+    ],
+    'modules' => [
+        'server' => [
+            'class' => \hipanel\modules\server\Module::class,
+        ],
+    ],
+    'components' => [
+        'i18n' => [
+            'translations' => [
+                'hipanel/server*' => [
+                    'class' => \yii\i18n\PhpMessageSource::class,
+                    'basePath' => '@hipanel/modules/server/messages',
+                    'fileMap' => [
+                        'hipanel/server' => 'server.php',
+                        'hipanel/server/os' => 'os.php',
+                        'hipanel/server/panel' => 'panel.php',
+                        'hipanel/server/rrd' => 'rrd.php',
+                    ],
+                ],
+                'omnilight/daterangepicker' => [ // TODO: get rid after PluginManager removing
+                    'class' => \yii\i18n\PhpMessageSource::class,
+                    'basePath' => '@omnilight/daterangepicker/messages',
+                    'sourceLanguage' => 'en-US',
+                ],
+            ],
+        ],
+        'menuManager' => [
+            'menus' => [
+                'server' => \hipanel\modules\server\SidebarMenu::class,
+            ],
+        ],
+    ],
+];
