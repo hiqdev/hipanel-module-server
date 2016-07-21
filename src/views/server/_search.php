e93ba240b6a57d18752be664378fef1c110d426d
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-07-21 15:33:07 +0300

    Removed Client and Seller filters from the AdvancedSearch view for non-support

--- src/views/server/_search.php
+++ src/views/server/_search.php
@@ -5,7 +5,9 @@
  * @var \hipanel\widgets\AdvancedSearch $search
  */
 use hipanel\modules\client\widgets\combo\ClientCombo;
+use hipanel\modules\client\widgets\combo\SellerCombo;
 use hipanel\widgets\RefCombo;
+
 ?>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
@@ -24,13 +26,15 @@ use hipanel\widgets\RefCombo;
     <?= $search->field('ip_like') ?>
 </div>
 
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('client_id')->widget(ClientCombo::class) ?>
-</div>
+<?php if (Yii::$app->user->can('support')) { ?>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?= $search->field('client_id')->widget(ClientCombo::class) ?>
+    </div>
 
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('seller_id')->widget(ClientCombo::class) ?>
-</div>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?= $search->field('seller_id')->widget(SellerCombo::class) ?>
+    </div>
+<?php } ?>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('hwsummary_like') ?>
