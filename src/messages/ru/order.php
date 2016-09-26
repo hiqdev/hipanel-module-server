2714ab016ee3e4a9bda6c759b56d68b79eb2c115
tofid <andreyklochok@gmail.com>
2016-09-26 18:01:22 +0300

    Added translations

--- src/messages/ru/order.php
+++ src/messages/ru/order.php
@@ -29,5 +29,8 @@ return [
     'Any social network link. Will be used in case of emergency contact' => 'Ссылка на любую социальную сеть. Будет использовано для срочной связи при необходимости.',
     'Server setup will be performed as soon as manager confirms your account verification. Pleas wait.' => 'Сервер будет создан сразу после того, как менеджер верифицирует Ваш аккаунт. Пожалуйста, ожидайте.',
     'You will receive an email with server access information right after setup' => 'Вы получите данные для доступа на сервера стразу после окончания установки.',
-    'Overuse {price}/{unit}' => 'Сверх объема тарифа: {price}/{unit}'
+    'Overuse {price}/{unit}' => 'Сверх объема тарифа: {price}/{unit}',
+    'Remote backup' => 'Удалённый бэкап',
+    'Backup overuse' => 'Перебор бэкапа',
+    'Backup usage' => 'Использование бэкапа',
 ];
