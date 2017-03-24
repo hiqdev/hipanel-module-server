8e0636d93dbc1d26ac6a449de1cffbec1b954e8e
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-03-24 14:20:11 +0200

    Added PHPUnit 6 support;

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -13,3 +13,15 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
+    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
+    {
+    }
+}
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
+
