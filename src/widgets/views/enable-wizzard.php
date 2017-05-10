85f6d62960eba6ca96eaaf36d6548f4ebe726acf
Yurii Myronchuk <bladeroot@gmail.com>
2017-05-10 17:19:37 +0200

    prettify

--- src/widgets/views/enable-wizzard.php
+++ src/widgets/views/enable-wizzard.php
@@ -7,24 +7,28 @@ use Yii;
 ?>
 
 <div class="col-md-3">
-    <?php $box = Box::begin(['renderBody' => false]) ?>
-    <?php $box->beginHeader() ?>
-    <?= $box->renderTitle(Yii::t('hipanel:server', 'Server')) ?>
-    <?php $box->endHeader() ?>
-    <?php $box->beginBody(); ?>
-    <?= $form->field($model, 'name')->input('text')->label(Yii::t('hipanel:server', 'Server')) ?>
-    <?= $form->field($model, 'dc')->input('text')->label(Yii::t('hipanel:server', 'DC')) ?>
-    <?php /* $form->field($model, 'type')->dropDownList($deviceTypes) **/ ?>
-    <?php $box->endBody(); ?>
-    <?php $box->end() ?>
+    <?php
+    $box = Box::begin(['renderBody' => false]);
+        $box->beginHeader();
+            echo $box->renderTitle(Yii::t('hipanel:server', 'Server'));
+        $box->endHeader();
+        $box->beginBody();
+            echo $form->field($model, 'name')->input('text')->label(Yii::t('hipanel:server', 'Server'));
+            echo $form->field($model, 'dc')->input('text')->label(Yii::t('hipanel:server', 'DC'));
+            /* $form->field($model, 'type')->dropDownList($deviceTypes) **/
+        $box->endBody();
+    $box->end()
+    ?>
 </div>
 <div class="col-md-9">
-    <?php $box = Box::begin(['renderBody' => false]) ?>
-    <?php $box->beginHeader() ?>
-    <?= $box->renderTitle(Yii::t('hipanel:server', 'Services')) ?>
-    <?php $box->endHeader() ?>
-    <?php $box->beginBody(); ?>
-    <?php $box->endBody(); ?>
-    <?php $box->end() ?>
+    <?php
+    $box = Box::begin(['renderBody' => false]);
+        $box->beginHeader();
+            echo $box->renderTitle(Yii::t('hipanel:server', 'Services'));
+        $box->endHeader();
+        $box->beginBody();
+        $box->endBody();
+    $box->end()
+    ?>
 </div>
 
