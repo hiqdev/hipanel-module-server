9fa3b6f30a0f8e66caf910143e8537f4ecf5c74c
Yurii Myronchuk <bladeroot@gmail.com>
2017-05-17 16:25:48 +0200

    cache VNC data

--- src/views/server/_vnc.php
+++ src/views/server/_vnc.php
@@ -1,49 +1,13 @@
 <?php
 use hipanel\widgets\Pjax;
-use yii\helpers\Html;
+use hipanel\modules\server\widgets\VNCOperation;
 
 Pjax::begin([
     'id' => 'server-vnc-pjax',
     'enablePushState' => false,
     'enableReplaceState' => false,
 ]);
-
-if ($model->vnc['enabled'] === true) {
-    echo Html::tag('span',
-        Html::tag('i', '', ['class' => 'glyphicon glyphicon-ok']) . ' ' . Yii::t('hipanel:server', 'Enabled'),
-        ['class' => 'label label-success']);
-
-    $fields = [
-        Yii::t('hipanel:server', 'IP') => $model->vnc['vnc_ip'],
-        Yii::t('hipanel:server', 'Port') => $model->vnc['vnc_port'],
-        Yii::t('hipanel:server', 'Password') => $model->vnc['vnc_password'],
-    ]; ?>
-    <dl class="dl-horizontal">
-        <?php foreach ($fields as $name => $value) : ?>
-            <dt><?= $name ?></dt>
-            <dd><?= $value ?></dd>
-        <?php endforeach ?>
-    </dl>
-    <?php if (!empty($model->vnc['endTime']) && $model->vnc['endTime'] > time()) {
-        echo Yii::t('hipanel:server', 'VNC will be disabled {time}',
-            ['time' => Yii::$app->formatter->asRelativeTime($model->vnc['endTime'])]);
-    }
-} else {
-    echo Html::beginForm(['enable-vnc', 'id' => $model->id], 'POST', ['data' => ['pjax' => 1], 'class' => 'inline']);
-    echo Html::submitButton(
-        Yii::t('hipanel:server', 'Enable'),
-        [
-            'class' => 'btn btn-success btn-block',
-            'data-loading-text' => Yii::t('hipanel:server', 'Enabling...'),
-            'onClick' => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')"),
-            'disabled' => !$model->canEnableVnc(),
-        ]
-    );
-    echo ' ';
-    if (!$model->isVNCSupported()) {
-        echo Yii::t('hipanel:server', 'VNC is supported only on XEN visualization');
-    }
-    echo Html::endForm();
-}
-
+echo VNCOperation::widget([
+    'model' => $model,
+]);
 Pjax::end();
---
+++ src/widgets/VNCOperation.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Server module for HiPanel.
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-server
+ * @package   hipanel-module-server
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\server\widgets;
+
+use Yii;
+use yii\base\Widget;
+use yii\helpers\Html;
+
+
+class VNCOperation extends Widget
+{
+
+    protected $vncEnd;
+    protected $vncEnabled;
+    public $model;
+
+    public function init()
+    {
+        parent::init();
+        if ($this->model === null) {
+            throw new InvalidConfigException('Please specify the "model" property.');
+        }
+
+        $this->vncEnabled = $this->model->vnc['enabled'];
+
+        $this->vncEnd = $this->model->statuses['serverEnableVNC'] === null ? 0 : strtotime('+8 hours', strtotime($this->model->statuses['serverEnableVNC']));
+        $this->vncEnd = $this->vncEnd < time() ? ($this->model->vnc['endTime'] > time() ? $this->model->vnc['endTime'] :  $this->vncEnd) :  $this->vncEnd;
+    }
+
+    public function run()
+    {
+        if ($this->model->isVNCSupported()) {
+            if ($this->vncEnabled === false) {
+                echo $this->renderEnableButton();
+            } else {
+                echo $this->renderToggleButton();
+            }
+        }
+    }
+
+    protected function renderEnableButton()
+    {
+        $form = Html::beginForm(['enable-vnc', 'id' => $this->model->id], 'POST', ['data' => ['pjax' => 1], 'class' => 'inline']);
+        $form .= Html::submitButton(
+            Yii::t('hipanel:server', 'Enable'),
+            [
+                'class' => 'btn btn-success btn-block',
+                'data-loading-text' => Yii::t('hipanel:server', 'Enabling...'),
+                'onClick' => new \yii\web\JsExpression("$(this).closest('form').submit(); $(this).button('loading')"),
+                'disabled' => !$this->model->canEnableVnc(),
+            ]
+        );
+        return $form . Html::endForm();
+    }
+
+    protected function renderToggleButton()
+    {
+        $data = Html::button(
+            Yii::t('hipanel', 'Show'),
+            [
+                'class' => 'btn btn-success btn-block',
+                'onClick' => new \yii\web\JsExpression("
+                    if ($('#vnc-access-data').hasClass('hidden')) {
+                        $(this).text('".Yii::t('hipanel', 'Hide') ."');
+                        $('#vnc-access-data').removeClass('hidden');
+                    } else {
+                        $(this).text('".Yii::t('hipanel', 'Show') ."');
+                        $('#vnc-access-data').addClass('hidden');
+                    }
+                "),
+            ]
+        );
+        $fields = [
+            Yii::t('hipanel:server', 'IP') => $this->model->vnc['vnc_ip'],
+            Yii::t('hipanel:server', 'Port') => $this->model->vnc['vnc_port'],
+            Yii::t('hipanel:server', 'Password') => $this->model->vnc['vnc_password'],
+        ];
+        $data .= Html::beginTag('dl', ['class' => 'dl-horizontal hidden', 'id' => 'vnc-access-data']);
+        foreach ($fields as $name => $value) {
+            $data .= Html::tag('dt', $name);
+            $data .= Html::tag('dd', $value);
+        }
+        $data .= Html::endTag('dl');
+        $data .= Yii::t('hipanel:server', 'VNC will be disabled {time}',
+                ['time' => Yii::$app->formatter->asRelativeTime($this->model->vnc['endTime'])]);
+        return $data;
+    }
+}
