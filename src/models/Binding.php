8cf182b9061a999df596136f2a557db5f5041cb0
tofid <andreyklochok@gmail.com>
2017-05-19 18:23:33 +0300

    Added rules to Binding model

--- src/models/Binding.php
+++ src/models/Binding.php
@@ -1,6 +1,21 @@
 <?php
+
 namespace hipanel\modules\server\models;
 
 class Binding extends \hipanel\base\Model
 {
+    use \hipanel\base\ModelTrait;
+
+    public static function tableName()
+    {
+        return 'server';
+    }
+
+    public function rules()
+    {
+        return [
+            [['device_id', 'switch_id'], 'integer'],
+            [['port', 'type', 'switch', 'switch_label', 'switch_inn', 'device_ip', 'switch_ip', 'web_iface_only'], 'string'],
+        ];
+    }
 }
