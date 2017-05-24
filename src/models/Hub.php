ae5f42adba517a756a7fbe6d028b77e905f5148b
tofid <andreyklochok@gmail.com>
2017-05-24 19:14:06 +0300

    Added requried field for create and update scenarios in Hub model

--- src/models/Hub.php
+++ src/models/Hub.php
@@ -9,9 +9,12 @@ class Hub extends \hipanel\base\Model
     public function rules()
     {
         return array_merge(parent::rules(), [
-            [['id', 'access_id', 'type_id', 'state_id'], 'integer'],
-            [['name', 'dc', 'mac', 'remoteid', 'note', 'ip', 'type_label'], 'string'],
+            [['id', 'access_id', 'type_id', 'state_id', 'buyer_id', 'units'], 'integer'],
+            [['name', 'dc', 'mac', 'remoteid', 'note', 'ip', 'type_label', 'buyer', 'note', 'inn', 'model',
+                'community', 'login', 'traf_server_id', 'order_no'], 'string'],
             [['virtual'], 'boolean'],
+
+            [['type_id', 'name'], 'required', 'on' => ['create', 'update']],
         ]);
     }
 }
