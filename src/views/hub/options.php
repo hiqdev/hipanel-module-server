e270334c6034b721ddfc70a4a5482436ea0b2e0c
tofid <andreyklochok@gmail.com>
2017-05-25 17:26:35 +0300

    Added title, breadcrumbs and render sub view to Hub options view

--- src/views/hub/options.php
+++ src/views/hub/options.php
@@ -1,1 +1,11 @@
 <?php
+
+use yii\bootstrap\Html;
+
+$this->title = Html::encode($model->name);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('hipanel:server', 'Switches'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+
+<?= $this->render('_optionsForm', compact(['model'])) ?>
