9609dd4ae302ffda8a230edf69cdac3e669e3c95
tofid <andreyklochok@gmail.com>
2017-05-25 17:23:50 +0300

    Added sesarch fields to Hub search

--- src/views/hub/_search.php
+++ src/views/hub/_search.php
@@ -1,7 +1,23 @@
 <?php
-
+/** @var array $types */
 ?>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('id') ?>
+    <?= $search->field('inn') ?>
 </div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('ip') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('mac') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('model') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('order_no') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('type_id')->dropDownList($types, ['prompt' => '--']) ?>
+</div>
+
