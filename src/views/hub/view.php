668ffe74a1213779795efe1a81a55c1cdb680778
tofid <andreyklochok@gmail.com>
2017-05-26 16:27:43 +0300

    Added new widget for details display

--- src/views/hub/view.php
+++ src/views/hub/view.php
@@ -1,11 +1,13 @@
 <?php
 
-use hipanel\modules\finance\grid\SaleGridView;
-use hipanel\modules\finance\grid\TariffGridView;
+use hipanel\modules\server\grid\HubGridView;
+use hipanel\modules\server\menus\HubDetailMenu;
+use hipanel\widgets\Box;
+use hipanel\widgets\MainDetails;
 use yii\helpers\Html;
 
-$this->title = Html::encode($model->object);
-$this->params['breadcrumbs'][] = ['label' => Yii::t('hipanel:finance:sale', 'Sale'), 'url' => ['index']];
+$this->title = Html::encode($model->name);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('hipanel:server', 'Switches'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
 $this->registerCss("
@@ -13,47 +15,66 @@ $this->registerCss("
         text-align: center;
     }
 ");
-
 ?>
 <div class="row">
-    <div class="col-md-4">
-        <div class="box box-solid">
-            <div class="box-header with-border">
-                <h3 class="box-title"><?= Yii::t('hipanel:finance:sale', 'Sale information') ?></h3>
-            </div>
-            <div class="box-body">
-                <?= SaleGridView::detailView([
-                    'model' => $model,
-                    'boxed' => false,
-                    'columns' => [
-                        'object_v',
-                        'time',
-                        'seller',
-                        'buyer',
-                        'tariff'
-                    ],
+    <div class="col-md-3">
+        <div class="row">
+            <div class="col-md-12">
+                <?= MainDetails::widget([
+                    'title' => $model->name,
+                    'icon' => 'fa-arrows-alt',
+                    'subTitle' => Html::a($model->buyer, ['@client/view', 'id' => $model->buyer_id]),
+                    'menu' => HubDetailMenu::widget(['model' => $model], ['linkTemplate' => '<a href="{url}" {linkOptions}><span class="pull-right">{icon}</span>&nbsp;{label}</a>']),
                 ]) ?>
+            </div>
+            <div class="col-md-12">
+                <?php
+                $box = Box::begin(['renderBody' => false]);
+                $box->beginHeader();
+                echo $box->renderTitle(Yii::t('hipanel:server', 'Switches'));
+                $box->endHeader();
+                $box->beginBody();
+                //                echo ServerGridView::detailView([
+                //                    'model' => $model,
+                //                    'boxed' => false,
+                //                    'columns' => [
+                //                        'net',
+                //                        'pdu',
+                //                        'rack',
+                //                        'ipmi',
+                //                    ]
+                //                ]);
+                $box->endBody();
+                $box->end();
+                ?>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <?= $this->render('@hipanel/modules/ticket/views/ticket/_clientInfo', compact('client')); ?>
-    </div>
-    <div class="col-md-4">
-        <div class="box box-solid">
-            <div class="box-header with-border">
-                <h3 class="box-title"><?= Yii::t('hipanel:finance:sale', 'Tariff information') ?></h3>
-            </div>
-            <div class="box-body">
-                <?= TariffGridView::detailView([
-                    'model' => $tariff,
+    <div class="col-md-9">
+        <div class="row">
+            <div class="col-md-6">
+                <?php
+                $box = Box::begin(['renderBody' => false]);
+                $box->beginHeader();
+                echo $box->renderTitle(Yii::t('hipanel:server:hub', 'General inforamtion'));
+                $box->endHeader();
+                $box->beginBody();
+                echo HubGridView::detailView([
+                    'model' => $model,
                     'boxed' => false,
                     'columns' => [
-                        'used',
-                        'note',
+                        'switch',
+                        'inn',
+                        'buyer',
+                        'model',
                         'type',
+                        'ip',
+                        'mac',
                     ],
-                ]) ?>
+                ]);
+                $box->endBody();
+                $box->end();
+                ?>
             </div>
         </div>
     </div>
