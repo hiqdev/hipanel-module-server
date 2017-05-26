2821750b5f8f9ca670eee8a4ff57e0252a0304e2
tofid <andreyklochok@gmail.com>
2017-05-26 16:26:57 +0300

    Added fields for options form in Hub

--- src/views/hub/options.php
+++ src/views/hub/options.php
@@ -1,5 +1,13 @@
 <?php
 
+/** @var array $snmpOptions */
+/** @var array $digitalCapacityOptions */
+/** @var array $nicMediaOptions */
+
+
+use hipanel\helpers\Url;
+use hipanel\widgets\PasswordInput;
+use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Html;
 
 $this->title = Html::encode($model->name);
@@ -8,4 +16,42 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<?= $this->render('_optionsForm', compact(['model'])) ?>
+<?php $form = ActiveForm::begin([
+    'id' => 'hub-options-form',
+    'validationUrl' => Url::toRoute(['validate-form', 'scenario' => $model->scenario]),
+]); ?>
+
+<div class="box">
+    <div class="box-body">
+        <div class="row">
+            <div class="col-md-4">
+                <?= $form->field($model, 'inn') ?>
+                <?= $form->field($model, 'model') ?>
+                <?= $form->field($model, 'login') ?>
+                <?= $form->field($model, 'password')->widget(PasswordInput::class) ?>
+            </div>
+            <div class="col-md-4">
+                <?= $form->field($model, 'ports_num') ?>
+                <?= $form->field($model, 'traf_server_id') ?>
+                <?= $form->field($model, 'vlan_server_id') ?>
+                <?= $form->field($model, 'community') ?>
+            </div>
+            <div class="col-md-4">
+                <?= $form->field($model, 'snmp_version_id')->dropDownList($snmpOptions, ['prompt' => '--']) ?>
+                <?= $form->field($model, 'digit_capacity_id')->dropDownList($digitalCapacityOptions, ['prompt' => '--']) ?>
+                <?= $form->field($model, 'nic_media')->dropDownList($nicMediaOptions, ['prompt' => '--']) ?>
+                <?= $form->field($model, 'base_port_no') ?>
+            </div>
+            <div class="col-md-12">
+                <?= $form->field($model, 'oob_key') ?>
+            </div>
+        </div>
+    </div>
+    <div class="box-footer">
+
+        <?= Html::submitButton(Yii::t('hipanel', 'Save'), ['class' => 'btn btn-success']) ?>
+        &nbsp;
+        <?= Html::button(Yii::t('hipanel', 'Cancel'), ['class' => 'btn btn-default', 'onclick' => 'history.go(-1)']) ?>
+    </div>
+</div>
+<?php $form->end() ?>
