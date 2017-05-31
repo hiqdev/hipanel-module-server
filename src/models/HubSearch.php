8734a275083e1e27043211215e6c2cf3864fe8b3
tofid <andreyklochok@gmail.com>
2017-05-31 19:24:36 +0300

    Added new search attribute `with_bindings`

--- src/models/HubSearch.php
+++ src/models/HubSearch.php
@@ -3,10 +3,21 @@
 namespace hipanel\modules\server\models;
 
 use hipanel\base\SearchModelTrait;
+use hipanel\helpers\ArrayHelper;
 
 class HubSearch extends Hub
 {
     use SearchModelTrait {
         searchAttributes as defaultSearchAttributes;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function searchAttributes()
+    {
+        return ArrayHelper::merge($this->defaultSearchAttributes(), [
+            'with_bindings',
+        ]);
     }
 }
