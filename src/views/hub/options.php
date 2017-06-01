462711c4b9cfbde62826de826a156c99e83786a1
tofid <andreyklochok@gmail.com>
2017-06-01 12:34:41 +0300

    Changed markup in hub options view

--- src/views/hub/options.php
+++ src/views/hub/options.php
@@ -24,39 +24,51 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <?= Html::activeHiddenInput($model, 'id') ?>
 
-<div class="box">
-    <div class="box-body">
-        <div class="row">
-            <div class="col-md-4">
-                <?= $form->field($model, 'inn') ?>
-                <?= $form->field($model, 'model') ?>
-                <?= $form->field($model, 'login') ?>
-                <?= $form->field($model, 'password')->widget(PasswordInput::class) ?>
+<div class="row">
+    <div class="col-md-6">
+        <div class="box">
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'inn') ?>
+                        <?= $form->field($model, 'model') ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'login') ?>
+                        <?= $form->field($model, 'password')->widget(PasswordInput::class) ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'ports_num') ?>
+                        <?= $form->field($model, 'traf_server_id')->widget(ServerCombo::class, [
+                            'pluginOptions' => [],
+                        ]) ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'vlan_server_id')->widget(ServerCombo::class) ?>
+                        <?= $form->field($model, 'community') ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'snmp_version_id')->dropDownList($snmpOptions, ['prompt' => '--']) ?>
+                        <?= $form->field($model, 'digit_capacity_id')->dropDownList($digitalCapacityOptions, ['prompt' => '--']) ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'nic_media')->dropDownList($nicMediaOptions, ['prompt' => '--']) ?>
+                        <?= $form->field($model, 'base_port_no') ?>
+                    </div>
+                    <div class="col-md-12">
+                        <?= $form->field($model, 'oob_key') ?>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'ports_num') ?>
-                <?= $form->field($model, 'traf_server_id')->widget(ServerCombo::class, [
-                    'pluginOptions' => [],
-                ]) ?>
-                <?= $form->field($model, 'vlan_server_id')->widget(ServerCombo::class) ?>
-                <?= $form->field($model, 'community') ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'snmp_version_id')->dropDownList($snmpOptions, ['prompt' => '--']) ?>
-                <?= $form->field($model, 'digit_capacity_id')->dropDownList($digitalCapacityOptions, ['prompt' => '--']) ?>
-                <?= $form->field($model, 'nic_media')->dropDownList($nicMediaOptions, ['prompt' => '--']) ?>
-                <?= $form->field($model, 'base_port_no') ?>
-            </div>
-            <div class="col-md-12">
-                <?= $form->field($model, 'oob_key') ?>
+            <div class="box-footer">
+
+                <?= Html::submitButton(Yii::t('hipanel', 'Save'), ['class' => 'btn btn-success']) ?>
+                &nbsp;
+                <?= Html::button(Yii::t('hipanel', 'Cancel'), ['class' => 'btn btn-default', 'onclick' => 'history.go(-1)']) ?>
             </div>
         </div>
-    </div>
-    <div class="box-footer">
 
-        <?= Html::submitButton(Yii::t('hipanel', 'Save'), ['class' => 'btn btn-success']) ?>
-        &nbsp;
-        <?= Html::button(Yii::t('hipanel', 'Cancel'), ['class' => 'btn btn-default', 'onclick' => 'history.go(-1)']) ?>
     </div>
 </div>
+
 <?php $form->end() ?>
