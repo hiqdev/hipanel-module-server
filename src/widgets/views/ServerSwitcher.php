36e5a3e85ac08e43619976ea93beb92c7d495a7d
tofid <andreyklochok@gmail.com>
2017-07-25 18:33:25 +0300

    Fixed margin bottom for server quick switcher

--- src/widgets/views/ServerSwitcher.php
+++ src/widgets/views/ServerSwitcher.php
@@ -1,20 +1,19 @@
 <?php
 
 use hipanel\modules\server\widgets\combo\ServerCombo;
-use yii\bootstrap\ActiveForm;
 
 ?>
-<div class="server-switcher">
-<?= ServerCombo::widget([
-    'model' => new \yii\base\DynamicModel(['name']),
-    'attribute' => 'name',
-    'hasId' => true,
-    'formElementSelector' => '.server-switcher',
-    'pluginOptions' => [
-        'select2Options' => [
-            'placeholder' => Yii::t('hipanel:server', 'Fast navigation to another server'),
-            'allowClear' => false,
+<div class="server-switcher" style="margin-bottom: 1em;">
+    <?= ServerCombo::widget([
+        'model' => new \yii\base\DynamicModel(['name']),
+        'attribute' => 'name',
+        'hasId' => true,
+        'formElementSelector' => '.server-switcher',
+        'pluginOptions' => [
+            'select2Options' => [
+                'placeholder' => Yii::t('hipanel:server', 'Fast navigation to another server'),
+                'allowClear' => false,
+            ],
         ],
-    ],
-]) ?>
+    ]) ?>
 </div>
