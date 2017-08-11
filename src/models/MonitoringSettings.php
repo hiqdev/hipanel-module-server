7f7d5620b42995c62df1544c691f751c8e43748e
tofid <andreyklochok@gmail.com>
2017-08-11 12:54:28 +0300

    Updated MonitoringSettings model, added rules, added defaults

--- src/models/MonitoringSettings.php
+++ src/models/MonitoringSettings.php
@@ -7,7 +7,24 @@ class MonitoringSettings extends \hipanel\base\Model
     public function rules()
     {
         return [
-            [['id', 'server', 'emails', 'minutes', 'nic_media', 'channel_load', 'watch_trafdown', 'vcdn_only', 'comment'], 'safe'],
+            [['id', 'nic_media', 'channel_load'], 'integer'],
+            [['watch_trafdown', 'vcdn_only'], 'boolean'],
+            [['server', 'emails', 'minutes', 'comment'], 'string'],
         ];
+    }
+
+    public function getEmailsDefault()
+    {
+        return 'notify@advancedhosters.com';
+    }
+
+    public function getNicMediaDefault()
+    {
+        return 40696899;
+    }
+
+    public function getChanelLoadDefault()
+    {
+        return 90;
     }
 }
