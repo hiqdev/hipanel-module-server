ea6e109ecab463b8d58bd12e4494a1ea0126b7bc
tofid <andreyklochok@gmail.com>
2017-09-01 15:57:53 +0300

    Fixed markup in _bulkSale view

--- src/views/server/_bulkSale.php
+++ src/views/server/_bulkSale.php
@@ -19,8 +19,12 @@ $defaultDateTime = new DateTime('first day of this month 00:00');
 <div>
     <!-- Nav tabs -->
     <ul class="nav nav-tabs" role="tablist">
-        <li role="presentation" class="active"><a href="#bulk" aria-controls="home" role="tab" data-toggle="tab"><?= Yii::t('hipanel', 'Set for all') ?></a></li>
-        <li role="presentation"><a href="#by-one" aria-controls="profile" role="tab" data-toggle="tab"><?= Yii::t('hipanel', 'Set by one') ?></a></li>
+        <li role="presentation" class="active">
+            <a href="#bulk" aria-controls="home" role="tab" data-toggle="tab"><?= Yii::t('hipanel', 'Set for all') ?></a>
+        </li>
+        <li role="presentation">
+            <a href="#by-one" aria-controls="profile" role="tab" data-toggle="tab"><?= Yii::t('hipanel', 'Set by one') ?></a>
+        </li>
     </ul>
 
     <!-- Tab panes -->
@@ -86,62 +90,72 @@ $defaultDateTime = new DateTime('first day of this month 00:00');
             </div>
 
             <div role="tabpanel" class="tab-pane" id="by-one">
-                <?php $form = ActiveForm::begin([
-                    'id' => 'bulk-by-one-sale',
-                    'action' => Url::toRoute('@server/bulk-sale'),
-                    'enableAjaxValidation' => true,
-                    'validateOnBlur' => true,
-                    'validationUrl' => Url::toRoute(['validate-form', 'scenario' => 'sale']),
-                ]); ?>
+                    <div class="col-md-12" style="margin-top: 15px;">
+                        <?php $form = ActiveForm::begin([
+                            'id' => 'bulk-by-one-sale',
+                            'action' => Url::toRoute('@server/bulk-sale'),
+                            'enableAjaxValidation' => true,
+                            'validateOnBlur' => true,
+                            'validationUrl' => Url::toRoute(['validate-form', 'scenario' => 'sale']),
+                        ]); ?>
 
-                <div class="col-md-12" style="margin-top: 15pt;">
-                    <?= $form->field($model, 'client_id')->widget(ClientCombo::class, [
-                        'options' => ['id' => 'bulk-by-one-sale-client'],
-                        'inputOptions' => [
-                            'id' => 'bulk-by-one-sale-client',
-                            'name' => 'client_id'
-                        ],
-                        'formElementSelector' => '.form-group',
-                    ]) ?>
 
-                    <?php foreach ($models as $model) : ?>
-                        <div class="col-md-3 text-right" style="line-height: 34px;">
-                            <?= Html::activeHiddenInput($model, "[$model->id]id") ?>
-                            <strong><?= $model->name ?></strong>
-                        </div>
-                        <div class="col-md-9">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <?= $form->field($model, "[$model->id]tariff_id")
-                                        ->widget(TariffCombo::class, [
-                                            'tariffType' => 'server',
-                                            'inputOptions' => ['ref' => 'tariff-combo']
-                                        ])
-                                        ->label(false)
-                                    ?>
-                                </div>
-                                <div class="col-md-6">
-                                    <?= $form->field($model, "[$model->id]sale_time")->widget(DateTimePicker::class, [
-                                        'pluginOptions' => [
-                                            'autoclose' => true,
-                                            'format' => 'yyyy-mm-dd hh:ii:ss',
-                                        ],
-                                        'options' => [
-                                            'value' => Yii::$app->formatter->asDatetime($defaultDateTime, 'php:Y-m-d H:i:s'),
-                                            'ref' => 'sale-time-combo'
-                                        ],
-                                    ])->label(false) ?>
-                                </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <?= $form->field($model, 'client_id')->widget(ClientCombo::class, [
+                                    'options' => ['id' => 'bulk-by-one-sale-client'],
+                                    'inputOptions' => [
+                                        'id' => 'bulk-by-one-sale-client',
+                                        'name' => 'client_id'
+                                    ],
+                                    'formElementSelector' => '.form-group',
+                                ]) ?>
                             </div>
+
+                            <?php foreach ($models as $model) : ?>
+                                <div class="col-md-3 text-right" style="line-height: 34px;">
+                                    <?= Html::activeHiddenInput($model, "[$model->id]id") ?>
+                                    <strong><?= $model->name ?></strong>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <?= $form->field($model, "[$model->id]tariff_id")
+                                                ->widget(TariffCombo::class, [
+                                                    'tariffType' => 'server',
+                                                    'inputOptions' => ['ref' => 'tariff-combo']
+                                                ])
+                                                ->label(false)
+                                            ?>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <?= $form->field($model, "[$model->id]sale_time")->widget(DateTimePicker::class, [
+                                                'pluginOptions' => [
+                                                    'autoclose' => true,
+                                                    'format' => 'yyyy-mm-dd hh:ii:ss',
+                                                ],
+                                                'options' => [
+                                                    'value' => Yii::$app->formatter->asDatetime($defaultDateTime, 'php:Y-m-d H:i:s'),
+                                                    'ref' => 'sale-time-combo'
+                                                ],
+                                            ])->label(false) ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+
                         </div>
-                    <?php endforeach; ?>
 
-                    <?= $form->field($model, 'move_accounts')->checkbox(['name' => 'move_accounts']) ?>
+                        <div class="col-md-12">
+                            <?= $form->field($model, 'move_accounts')->checkbox(['name' => 'move_accounts']) ?>
 
-                    <hr>
-                    <?= Html::submitButton(Yii::t('hipanel:server', 'Sell'), ['class' => 'btn btn-success', 'id' => 'save-button']) ?>
-                    <?php ActiveForm::end() ?>
-                </div>
+                            <hr>
+                            <?= Html::submitButton(Yii::t('hipanel:server', 'Sell'), ['class' => 'btn btn-success', 'id' => 'save-button']) ?>
+                            <?php ActiveForm::end() ?>
+                        </div>
+
+                    </div>
+
             </div>
         </div>
     </div>
