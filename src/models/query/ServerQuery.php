4e0c252eb3e4dcff103766ffddaf4be0fcfbb56a
Yurii Myronchuk <bladeroot@gmail.com>
2018-11-26 09:03:23 +0000

    fix access to hardware-settings

--- src/models/query/ServerQuery.php
+++ src/models/query/ServerQuery.php
@@ -43,7 +43,7 @@ class ServerQuery extends ActiveQuery
 
     public function withHardwareSettings(): self
     {
-        if (Yii::$app->user->can('admin')) {
+        if (Yii::$app->user->can('part.read')) {
             $this->joinWith(['hardwareSettings']);
             $this->andWhere(['with_hardwareSettings' => 1]);
         }
