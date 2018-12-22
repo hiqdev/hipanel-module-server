cd81f9bde10bff3d11fd480be41b42ec93542c60
Andrey Klochok <andreyklochok@gmail.com>
2018-12-22 12:10:47 +0200

    Fixed consumption price display (#55)
    * Fixed consumption price display
    
    * minor

---
+++ src/helpers/consumptionPriceFormatter/SinglePrice.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace hipanel\modules\server\helpers\consumptionPriceFormatter;
+
+
+use hipanel\modules\server\models\Consumption;
+use Yii;
+
+class SinglePrice implements PriceFormatterStrategy
+{
+    /**
+     * @param Consumption $consumption
+     * @return string
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function showPrice(Consumption $consumption): string
+    {
+        return Yii::$app->formatter->asCurrency($consumption->price, $consumption->currency);
+    }
+}
