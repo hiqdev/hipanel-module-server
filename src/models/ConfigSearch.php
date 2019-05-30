ccd0dd97a79078cb9c15672ed9288185ac9ef730
Maksim Gayduk <maksim.gayduk@gmail.com>
2019-05-30 16:17:15 +0300

    Created Advanced search box

--- src/models/ConfigSearch.php
+++ src/models/ConfigSearch.php
@@ -10,11 +10,51 @@
 namespace hipanel\modules\server\models;
 
 use hipanel\base\SearchModelTrait;
+use hipanel\helpers\ArrayHelper;
+use Yii;
 
 
 class ConfigSearch extends Config
 {
     use SearchModelTrait {
         searchAttributes as defaultSearchAttributes;
+    }
+
+    public function searchAttributes()
+    {
+        return ArrayHelper::merge($this->defaultSearchAttributes(), [
+            'name_ilike',
+            'subname_ilike',
+            'location_ilike',
+            'cpu_ilike',
+            'ram_ilike',
+            'hdd_ilike',
+            'traffic_ilike',
+            'lan_ilike',
+            'raid_ilike',
+            'sort_order_ilike',
+            'price_ilike',
+            'last_price_ilike',
+            'description_ilike',
+        ]);
+    }
+
+    public function attributeLabels()
+    {
+        return array_merge(parent::attributeLabels(), [
+            'name_ilike'        => Yii::t('hipanel:server', 'Name'),
+            'subname_ilike'     => Yii::t('hipanel:server:config', 'Subname'),
+            'location_ilike'    => Yii::t('hipanel:server:config', 'Location'),
+            'cpu_ilike'         => Yii::t('hipanel:server:config', 'CPU'),
+            'ram_ilike'         => Yii::t('hipanel:server:config', 'RAM'),
+            'hdd_ilike'         => Yii::t('hipanel:server:config', 'HDD'),
+            'traffic_ilike'     => Yii::t('hipanel:server:config', 'Traffic'),
+            'lan_ilike'         => Yii::t('hipanel:server:config', 'LAN'),
+            'raid_ilike'        => Yii::t('hipanel:server:config', 'RAID'),
+            'sort_order_ilike'  => Yii::t('hipanel:server:config', 'Sort order'),
+            'price_ilike'       => Yii::t('hipanel:server:config', 'Price'),
+            'last_price_ilike'  => Yii::t('hipanel:server:config', 'Last price'),
+            'description_ilike' => Yii::t('hipanel:server:config', 'Description'),
+        ]);
     }
 }
--- src/views/config/_search.php
+++ src/views/config/_search.php
@@ -1,2 +1,43 @@
 <?php
+/** @var \hipanel\widgets\AdvancedSearch $search */
+?>
 
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('name_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('subname_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('location_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('cpu_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('ram_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('hdd_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('traffic_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('lan_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('raid_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('sort_order_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('price_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('last_price_ilike') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('description_ilike') ?>
+</div>
