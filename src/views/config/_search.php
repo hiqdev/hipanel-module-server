4b68e30e6fb0ac30f17778152fd2ebad4c6299e4
Andrii Vasyliev <sol@hiqdev.com>
2019-06-03 10:32:29 +0000

    Fixed filters in config index

--- src/views/config/_search.php
+++ src/views/config/_search.php
@@ -1,5 +1,10 @@
 <?php
+
 /** @var \hipanel\widgets\AdvancedSearch $search */
+
+use hipanel\modules\client\widgets\combo\ClientCombo;
+use hipanel\modules\client\widgets\combo\SellerCombo;
+
 ?>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
@@ -9,29 +14,13 @@
     <?= $search->field('subname_ilike') ?>
 </div>
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('cpu_ilike') ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('ram_ilike') ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('hdd_ilike') ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('ssd_ilike') ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('traffic_ilike') ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('lan_ilike') ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('raid_ilike') ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('sort_order') ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('descr_ilike') ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('client_id')->widget(ClientCombo::class, ['formElementSelector' => '.form-group']) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('seller_id')->widget(SellerCombo::class, ['formElementSelector' => '.form-group']) ?>
 </div>
