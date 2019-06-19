25e8dab7bc368df52fc1df195c8424d46899983a
Andrey Klochok <andreyklochok@gmail.com>
2019-06-19 13:20:43 +0300

    Fixed Config search for Cart, changed `search` to `get-available`

--- src/cart/ServerOrderDedicatedProduct.php
+++ src/cart/ServerOrderDedicatedProduct.php
@@ -81,7 +81,7 @@ class ServerOrderDedicatedProduct extends AbstractServerProduct
     /** {@inheritdoc} */
     private function ensureRelatedData()
     {
-        $this->_model = Config::findOne($this->object_id);
+        $this->_model = Config::find(['batch' => true])->getAvailable()->withSellerOptions()->andWhere(['id' => $this->object_id])->one();
         $this->_image = Osimage::find()->where(['osimage' => $this->osimage, 'type' => 'dedicated'])->one();
         $this->name = $this->_model->name;
         $this->description = $this->_model->descr;
