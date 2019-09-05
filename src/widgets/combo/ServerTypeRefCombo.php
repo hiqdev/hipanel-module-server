d28482c88e18ea21c78fab5dffbb48ebd5e7d975
Andrey Klochok <andreyklochok@gmail.com>
2019-09-05 15:19:46 +0300

    Showed more server types by default to all clients (#133)

--- src/widgets/combo/ServerTypeRefCombo.php
+++ src/widgets/combo/ServerTypeRefCombo.php
@@ -7,15 +7,18 @@ use Yii;
 
 class ServerTypeRefCombo extends RefCombo
 {
-    public function prepareData()
+    /**
+     * @return array
+     */
+    public function prepareData(): array
     {
         $refs = parent::prepareData();
         if (Yii::$app->user->can('ref.view.not-used')) {
             return $refs;
         }
 
-        return array_filter($refs, function ($k) {
-            return in_array($k, ['dedicated', 'unmanaged']);
+        return array_filter($refs, static function (string $k): bool {
+            return in_array($k, ['dedicated', 'unmanaged', 'setup', 'jbod', 'remote', 'vds', 'avds', 'ovds', 'svds', 'cdn', 'cdnpix', 'nic', 'uplink3'], true);
         }, ARRAY_FILTER_USE_KEY);
     }
 }
