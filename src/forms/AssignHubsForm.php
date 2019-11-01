4354c13e9616e18b9da5256dca45c2ce2f97ad2d
strorch <smy980807@ukr.net>
2019-11-01 17:56:25 +0200

    minor

--- src/forms/AssignHubsForm.php
+++ src/forms/AssignHubsForm.php
@@ -24,7 +24,7 @@ class AssignHubsForm extends Server
     /**
      * @var array
      */
-    protected $switchVariants = ['net', 'kvm', 'pdu', 'rack', 'pdu2', 'ipmi', 'net2'];
+    protected $switchVariants = ['net', 'kvm', 'pdu', 'rack', 'pdu2', 'net2', 'ipmi'];
 
     /**
      * {@inheritdoc}
