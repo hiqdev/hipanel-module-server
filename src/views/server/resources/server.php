31d9e128d2124cca5532acb95685bc82d3d3e1f5
Andrey Klochok <andreyklochok@gmail.com>
2021-03-23 11:56:28 +0200

    Add detail info about server to resrource detail view

--- src/views/server/resources/server.php
+++ src/views/server/resources/server.php
@@ -2,6 +2,7 @@
 
 use hipanel\models\IndexPageUiOptions;
 use hipanel\modules\finance\widgets\ResourceDetailViewer;
+use hipanel\modules\server\grid\ServerGridView;
 use hiqdev\hiart\ActiveDataProvider;
 use yii\db\ActiveRecordInterface;
 
@@ -17,11 +18,37 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<?= ResourceDetailViewer::widget([
-    'dataProvider' => $dataProvider,
-    'originalContext' => $this->context,
-    'originalModel' => $originalModel,
-    'originalSearchModel' => $model,
-    'uiModel' => $uiModel,
-    'configurator' => Yii::$container->get('server-resource-config'),
-]) ?>
+<div class="row">
+    <div class="col-md-3">
+        <div class="box box-widget">
+            <div class="box-header with-border">
+                <h3 class="box-title"><?= $originalModel->dc ?></h3>
+            </div>
+            <div class="box-body no-padding">
+                <?= ServerGridView::detailView([
+                    'boxed' => false,
+                    'model' => $originalModel,
+                    'columns' => [
+                        'client_id', 'seller_id',
+                        [
+                            'attribute' => 'name',
+                            'contentOptions' => ['class' => 'text-bold'],
+                        ], 'detailed_type',
+                        'ip', 'note', 'label',
+                        'mails_num',
+                    ],
+                ]) ?>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-9">
+        <?= ResourceDetailViewer::widget([
+            'dataProvider' => $dataProvider,
+            'originalContext' => $this->context,
+            'originalModel' => $originalModel,
+            'originalSearchModel' => $model,
+            'uiModel' => $uiModel,
+            'configurator' => Yii::$container->get('server-resource-config'),
+        ]) ?>
+    </div>
+</div>
