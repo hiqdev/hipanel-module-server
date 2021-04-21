7749df1b11b4f8a371bcd76828ea33dbfec1c42e
Maksym Storchak <smy980807@ukr.net>
2021-04-21 19:22:37 +0300

    added LocationCombo (#172)
    * added LocationCombo
    
    * minor

---
+++ src/widgets/combo/LocationCombo.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace hipanel\modules\server\widgets\combo;
+
+use hipanel\helpers\ArrayHelper;
+use hiqdev\combo\Combo;
+
+class LocationCombo extends Combo
+{
+    /** {@inheritdoc} */
+    public $type = 'server/hub';
+
+    /** {@inheritdoc} */
+    public $name = 'name';
+
+    /** {@inheritdoc} */
+    public $url = '/server/hub/index';
+
+    /** {@inheritdoc} */
+    public $_return = ['id'];
+
+    /** {@inheritdoc} */
+    public $_rename = ['text' => 'name'];
+
+    /** {@inheritdoc} */
+    public function getFilter()
+    {
+        return ArrayHelper::merge(parent::getFilter(), [
+            'type_in'  => ['format' => 'switch,location'],
+            'limit' => ['format' => '50'],
+        ]);
+    }
+}
