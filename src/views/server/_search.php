05c8574bfc90730bc10bfebed28a327dca7a07f4
Yurii Myronchuk <bladeroot@hiqdev.com>
2022-06-01 05:47:15 +0000

    add filter rack_inilike

--- src/views/server/_search.php
+++ src/views/server/_search.php
@@ -97,7 +97,7 @@ use hipanel\widgets\AdvancedSearch;
     </div>
 
     <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('rack_ilike') ?>
+        <?= $search->field('rack_inilike') ?>
     </div>
 
     <div class="col-md-4 col-sm-6 col-xs-12">
