6d82bd0b5a253e6397fd9cbae0b922787926ee33
gadezist <andrybitko@gmail.com>
2022-06-28 18:30:54 +0300

    HP-858 fix tests and skip ServerOrder test. Fix bugs that i found during testing

--- tests/acceptance/seller/OrderServerCest.php
+++ tests/acceptance/seller/OrderServerCest.php
@@ -11,10 +11,26 @@
 namespace hipanel\modules\server\tests\acceptance\seller;
 
 use hipanel\helpers\Url;
+use hipanel\modules\server\tests\_support\Helper\OrderServerHelper;
+use hipanel\tests\_support\Step\Acceptance\Manager;
 use hipanel\tests\_support\Step\Acceptance\Seller;
 
 class OrderServerCest
 {
+    private OrderServerHelper $orderServerHelper;
+
+    protected function _inject(OrderServerHelper $orderServerHelper): void
+    {
+        $this->orderServerHelper = $orderServerHelper;
+    }
+
+    public function _before(Manager $I, $scenario): void
+    {
+        if (!$this->orderServerHelper->canSeeOrderServer()) {
+            $scenario->skip($this->orderServerHelper->getDisabledMessage());
+        }
+    }
+
     /**
      * @skip
      */
