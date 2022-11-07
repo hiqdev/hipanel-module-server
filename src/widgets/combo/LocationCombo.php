ca3b7719eef3f4779f4345f1122724f171a70230
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2022-11-07 11:55:27 +0200

    HP-1121 Include region in location combo

--- src/widgets/combo/LocationCombo.php
+++ src/widgets/combo/LocationCombo.php
@@ -27,7 +27,7 @@ class LocationCombo extends Combo
     public function getFilter()
     {
         return ArrayHelper::merge(parent::getFilter(), [
-            'type_in'  => ['format' => 'switch,location'],
+            'type_in'  => ['format' => 'switch,location,region'],
             'limit' => ['format' => '50'],
         ]);
     }
