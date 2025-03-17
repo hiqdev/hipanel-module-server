4358b230985bd59e296d9a6b6a95fa1f3b9cfd34
Andrii Klochok <andreyklochok@gmail.com>
2025-03-17 21:22:35 +0200

    HP-2334: add delivery time

--- src/views/server/modal/softwareSettings.php
+++ src/views/server/modal/softwareSettings.php
@@ -1,6 +1,9 @@
 <?php
 
+/** @var Server $model */
+
 use hipanel\helpers\Url;
+use hipanel\modules\server\models\Server;
 use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Html;
 
@@ -19,6 +22,9 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <?= $form->field($model->softwareSettings, 'os') ?>
 <?= $form->field($model->softwareSettings, 'version') ?>
+<?= $form->field($model->softwareSettings, 'delivery_time')
+    ->input('number', ['step' => 1, 'min' => 0])
+    ->hint(Yii::t('hipanel:server', 'The unit of measurement is an hour.')) ?>
 <?= $form->field($model->softwareSettings, 'virtual_switch')->checkbox() ?>
 <?= $form->field($model->softwareSettings, 'ignore_ip_mon')->checkbox() ?>
 <?= $form->field($model->softwareSettings, 'ip_mon_comment') ?>
