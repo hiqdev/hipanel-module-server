51605eff0613f5e2ed3cbb9e73cb0988a6d9685b
Andrii Klochok <andreyklochok@gmail.com>
2025-09-22 19:32:03 +0300

    HP-2679: introduce ApplyToAllWidget with associated JS and assets, update AssignHubsPage to use the new widget

---
+++ src/assets/ApplyToAllAsset.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace hipanel\modules\server\assets;
+
+use yii\web\AssetBundle;
+use yii\web\JqueryAsset;
+
+class ApplyToAllAsset extends AssetBundle
+{
+    public $sourcePath = __DIR__ . '/js';
+    public $js = ['ApplyToAll.js'];
+    public $depends = [
+        JqueryAsset::class,
+    ];
+    public $publishOptions = ['only' => ['ApplyToAll.js']];
+}
