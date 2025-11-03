3d4b62ba072e3eee5b1689cbc28edafd506b0868
Andrii Klochok <andreyklochok@gmail.com>
2025-11-03 14:49:34 +0200

    HP-2665: add new export tests for server and hub

---
+++ tests/playwright/e2e/reseller/server/export.spec.ts
@@ -0,0 +1,9 @@
+import { test } from "@hipanel-core/fixtures";
+import Index from "@hipanel-core/page/Index";
+
+test("hub export works correctly @hipanel-module-server @seller", async ({ page }) => {
+  const indexPage = new Index(page);
+
+  await page.goto("/server/server/index");
+  await indexPage.testExport();
+});
